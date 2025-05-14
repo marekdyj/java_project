5fc382e3e63f555b7e133739846bce261cfd2d90
simpleMDMS <simpleMDMS@gmail.com>
2025-05-14 17:36:43 +0200

    Stworzono logikę tetrimino

---
+++ src/main/java/com/example/tetris_test_v1/Position.java
@@ -0,0 +1,17 @@
+package com.example.tetris_test_v1;
+
+public class Position{
+    public int x;
+    public int y;
+    Position(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+    public void setY(int y) {
+        this.y = y;
+    }
+}
